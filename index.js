48ab0dcc906f38d7c072c1e1c4d439c9a4b2827a
Jay Versluis <versluis2000@yahoo.com>
2019-09-04 18:28:45 -0400

    increased timeout for copied character message

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
+// UMLAUTBOARD - Externalised JS
 
+var st, char;
+
+function init()
+{
+	message = document.getElementById('message');
+}
+
+function copyChar(el)
+{
+	var oldContentEditable = el.contentEditable,
+		oldReadOnly = el.readOnly,
+		range = document.createRange();
+
+	el.contentEditable = true;
+	el.readOnly = false;
+	range.selectNodeContents(el);
+
+	var s = window.getSelection();
+	s.removeAllRanges();
+	s.addRange(range);
+
+	el.setSelectionRange(0, 999999); // A big number, to cover anything that could be inside the element.
+
+	el.contentEditable = oldContentEditable;
+	el.readOnly = oldReadOnly;
+
+	document.execCommand('copy');
+	// window.scrollTo(0, 0);
+	window.scrollTo(0,document.body.scrollHeight);
 	
-		var st, char;
+	clearTimeout(st);
+	message.innerHTML = 'Copied ' + el.value + ' to pasteboard.';
+	st = setTimeout('clearMessage()', 3500);			
+}
 
-		function init()
-		{
-			message = document.getElementById('message');
-		}
-
-		function copyChar(el)
-		{
-		    var oldContentEditable = el.contentEditable,
-		        oldReadOnly = el.readOnly,
-		        range = document.createRange();
-		
-		    el.contentEditable = true;
-		    el.readOnly = false;
-		    range.selectNodeContents(el);
-		
-		    var s = window.getSelection();
-		    s.removeAllRanges();
-		    s.addRange(range);
-		
-		    el.setSelectionRange(0, 999999); // A big number, to cover anything that could be inside the element.
-		
-		    el.contentEditable = oldContentEditable;
-		    el.readOnly = oldReadOnly;
-		
-			document.execCommand('copy');
-			// window.scrollTo(0, 0);
-			window.scrollTo(0,document.body.scrollHeight);
-			
-			clearTimeout(st);
-			message.innerHTML = 'Copied ' + el.value + ' to pasteboard.';
-			st = setTimeout('clearMessage()', 2500);			
-		}
-		
-		function clearMessage()
-		{
-			message.innerHTML = '';
-		}
+function clearMessage()
+{
+	message.innerHTML = '';
+}
 		
 	
 
